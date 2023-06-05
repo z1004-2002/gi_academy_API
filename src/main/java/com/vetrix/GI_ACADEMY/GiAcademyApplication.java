83c998b5fa5bb3cbaf6ca83a3c78f99af3c58eab
abel <zogningfranc@gmail.com>
2023-06-05 18:19:21 +0100

    Backend du projet Poo

---
+++ src/main/java/com/vetrix/GI_ACADEMY/GiAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.vetrix.GI_ACADEMY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiAcademyApplication.class, args);
+	}
+
+}
